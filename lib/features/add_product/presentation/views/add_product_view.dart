7750e65d7213711fe8c1557265577c6bbab92ddf
Ahmed Rizk <rezk18649@gmail.com>
2025-09-01 19:11:32 +0300

    create add product view

---
+++ lib/features/add_product/presentation/views/add_product_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub_dashboard/features/add_product/presentation/views/widgets/add_product_view_body.dart';
+
+class AddProductView extends StatelessWidget {
+  const AddProductView ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AddProductViewBody(),
+    );
+  }
+}
